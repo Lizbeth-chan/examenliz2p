3044f0682c9fa98c8e3b7da30c68ca11efb4959f
Lizbeth-chan <A01271871@itesm.mx>
2017-11-05 01:51:23 -0600

    SolucionGuardar

--- app/components/form-test.js
+++ app/components/form-test.js
@@ -2,22 +2,23 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   store: Ember.inject.service('store'),
-  save(){
-    let test= this.get('test');
 
-    if(Ember.isBlank(test.get('nombre'))){
-      alert('El campo no puede estar vacio');
-      return;
-    }
-
-    test.save().then(()=>{
-    Ember.RSVP.all( this.get('test.questions').invoke('save') ).then(()=>{
-        alert('Ya se guardo');
-      this.sendAction('guardar');
-      })
-    });
-  },
   actions:{
+    save(){
+      let test= this.get('test');
+
+      if(Ember.isBlank(test.get('nombre'))){
+        alert('El campo no puede estar vacio');
+        return;
+      }
+
+      test.save().then(()=>{
+      Ember.RSVP.all( this.get('test.questions').invoke('save') ).then(()=>{
+          alert('Ya se guardo');
+        this.sendAction('guardar');
+        })
+      });
+    },
     crearPregunta(){
       let question = this.get('store').createRecord('question', {
         test: this.get('test'),
